a973b7d5bee4ed265d350a016a7bdcfae42b1986
Bastien <bastien@alfa-safety.fr>
2017-10-27 10:57:24 +0200

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir /var/lib/app && cp -r /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/* /var/lib/app/
 
-cd /var/lib/app && npm install && npm install strongloop
+cd /var/lib/app && npm install && npm install -g strongloop
