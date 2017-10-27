38a1d4ec2654b808687af71ef19b146c6c1697fe
Bastien <bastien@alfa-safety.fr>
2017-10-27 11:29:05 +0200

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir /var/lib/app && cp -r /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/* /var/lib/app/
 
-cd /var/lib/app && npm install && npm install -g forever
+cd /var/lib/app && npm install > loginstall.txt && npm install -g forever > logforever.txt
